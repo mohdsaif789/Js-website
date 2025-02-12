8fb7e4a46551e769c3d8d8743bac37cc6c8b50ce
mohdsaif789 <mohdsaif.saif2008@gmail.com>
2025-02-12 21:28:00 +0530

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,25 @@
+html{
+    background-color: pink;
+}
+#create{
+    background-color: black;
+    color: white;
+    font-family: cursive;
+    text-align: center;
+}
+#liner{
+    background-color: bisque;
+    color: black;
+    font-family: sans-serif;
+    font-size: 20px;
+}
+#plug{
+    background-color: aqua;
+    color: black;
+    border-radius: 5px;
+}
+#pin{
+    background-color: skyblue;
+    color: black;
+    border-radius: 5px;
+}
